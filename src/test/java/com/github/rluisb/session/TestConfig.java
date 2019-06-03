3362107db4da93a3456b623081ebcda05b83ed54
Ricardo Conceição <ricardo220893@gmail.com>
2019-06-03 00:04:07 -0300

    Fixing TestConfig

--- src/test/java/com/github/rluisb/session/TestConfig.java
+++ src/test/java/com/github/rluisb/session/TestConfig.java
@@ -1,12 +1,12 @@
 package com.github.rluisb.session;
 
 
-import com.github.rluisb.agenda.cucumber.World;
+import com.github.rluisb.session.cucumber.World;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
         classes = {
-                AgendaServiceApplication.class,
+                SessionServiceApplication.class,
                 World.class
         })
 public class TestConfig {
